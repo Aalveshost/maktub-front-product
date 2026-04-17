a2dbe7f81ccaafa322a527eeedb1ea890c1c49ca
Aalves <aalves.host@gmail.com>
2026-04-17 13:26:36 -0300

    Update v1.3.57: Finalized UI restoration with Sorvetes integration

--- maktub-front-product.php
+++ maktub-front-product.php
@@ -3,7 +3,7 @@
  * Plugin Name: Maktub Front Product Manager
  * Plugin URI: https://github.com/Aalveshost/maktub-front-product
  * Description: Interface premium para edição de produtos (Preço e Status) no frontend. Integrado com Jet Engine e WooCommerce.
- * Version: 1.3.56
+ * Version: 1.3.57
  * Author: Alef Alves
  * Author URI: https://aalves.dev
  * Text Domain: maktub-front
@@ -32,7 +32,7 @@ final class Maktub_Front_Product {
 	}
 
 	private function define_constants() {
-		define( 'MAKTUB_FRONT_VERSION', '1.3.56' );
+		define( 'MAKTUB_FRONT_VERSION', '1.3.57' );
 		define( 'MAKTUB_FRONT_PATH', plugin_dir_path( __FILE__ ) );
 		define( 'MAKTUB_FRONT_URL', plugin_dir_url( __FILE__ ) );
 	}
